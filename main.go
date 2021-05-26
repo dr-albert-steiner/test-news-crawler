9582755eeeb5542820459c23c9368a933028b674
Dr Albert Steiner <stanislav.vlasko@gmail.com>
2021-05-26 15:07:33 +0400

    api was added

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/api/news", newsHandler)
+	http.HandleFunc("/api/url", urlHandler)
+	http.HandleFunc("/api/pattern", patternHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func newsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, fmt.Sprintf("Method %s is not supported", r.Method), http.StatusNotFound)
+	}
+	fmt.Println("news GET")
+}
+
+func urlHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		fmt.Println("URLs GET")
+	case "POST":
+		fmt.Println("URLs POST")
+	case "DELETE":
+		fmt.Println("URLs DELETE")
+	default:
+		http.Error(w, fmt.Sprintf("Method %s is not supported", r.Method), http.StatusNotFound)
+	}
+}
+
+func patternHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		fmt.Println("patterns GET")
+	case "POST":
+		fmt.Println("patterns POST")
+	case "DELETE":
+		fmt.Println("patterns DELETE")
+	default:
+		http.Error(w, fmt.Sprintf("Method %s is not supported", r.Method), http.StatusNotFound)
+	}
+}
